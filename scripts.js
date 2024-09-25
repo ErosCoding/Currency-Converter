d0f22992e1bbe1bed5361fc46b8bad39bbf48266
Eros <sickoferos@gmail.com>
2024-09-25 17:22:16 -0300

    obtendo a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
+// Obtendo os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
-  
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
- 
 })
+
+// Capturando o evento de submit(envio) no formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  
+}
+
 
