fb0a32738582f981c4f9b29af9e9ea273aca4b8c
Eros <sickoferos@gmail.com>
2024-09-25 17:30:44 -0300

    identificando a moeda e o valor

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Cotação de moedas do dia.
+const USD = 5.48
+const EUR = 6.10
+const GBP = 7.30
+
 // Obtendo os elementos do formulário
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
@@ -12,8 +17,23 @@ amount.addEventListener("input", () => {
 // Capturando o evento de submit(envio) no formulário
 form.onsubmit = (event) => {
   event.preventDefault()
-
   
+  switch(currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+}
+
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
 }
 
 
