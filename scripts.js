4e272736b196d8d4fb53563c7918fafd6034222a
Eros <sickoferos@gmail.com>
2024-09-25 17:16:09 -0300

    validando input

--- scripts.js
+++ scripts.js
@@ -2,5 +2,9 @@ const amount = document.getElementById("amount")
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
-  console.log(amount.value)
+  
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+ 
 })
+
